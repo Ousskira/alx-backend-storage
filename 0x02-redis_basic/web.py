d05852a733ad5dbe3f07115115ef33118f53b3e6
Oussama EL GUENNOUNI <138916850+Ousskira@users.noreply.github.com>
2024-08-14 14:36:21 +0000

    Redis_Basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -23,8 +23,8 @@ def data_cacher(method: Callable) -> Callable:
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
+        
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
